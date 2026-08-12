dd4be244f0adc5e4e6cfa6b230c51eefdc1f64cd
Excellencedev <ademiluyisuccessandexcellence@gmail.com>
2026-08-12 16:39:26 -0600

    Revert "Disable unstable worldgen biome injection during startup"

--- src/main/java/com/adventuremod/world/ModWorldGen.java
+++ src/main/java/com/adventuremod/world/ModWorldGen.java
@@ -1,9 +1,16 @@
 package com.adventuremod.world;
 
 import com.adventuremod.AdventureMod;
+import com.adventuremod.entity.ModEntities;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 
@@ -16,6 +23,33 @@ public class ModWorldGen {
 
     public static void registerWorldGen() {
         AdventureMod.LOGGER.info("Registering World Gen for " + AdventureMod.MOD_ID);
-        AdventureMod.LOGGER.info("World generation biome injection is temporarily disabled while startup loading is stabilized.");
+        ModBiomes.registerBiomes();
+
+        BiomeModifications.addFeature(
+                BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.SURFACE_STRUCTURES,
+                RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(AdventureMod.MOD_ID, "hunter_outpost"))
+        );
+
+        BiomeModifications.addSpawn(
+                BiomeSelectors.foundInOverworld(),
+                SpawnGroup.CREATURE,
+                ModEntities.WILD_BOAR,
+                60, 2, 4
+        );
+
+        BiomeModifications.addSpawn(
+                BiomeSelectors.foundInOverworld(),
+                SpawnGroup.CREATURE,
+                ModEntities.DEER,
+                80, 2, 5
+        );
+
+        BiomeModifications.addSpawn(
+                BiomeSelectors.foundInOverworld(),
+                SpawnGroup.MISC,
+                ModEntities.GUARD_VILLAGER,
+                5, 1, 1
+        );
     }
 }
